83bc823e99ecf4f695e839c99e13ba0fbd3a7bb4
ThomasJ99 <xmaslink1999@gmail.com>
2026-01-09 15:17:13 +0100

    Version 2, inspired by LoZ

--- dist/adventure.js
+++ dist/adventure.js
@@ -1,51 +1,106 @@
 "use strict";
-// --- Version 1 av Äventyr ---
+//*** --- Version 2 av Äventyr --- ***//
 // -- Steg 1: Hjälten --
-const heroName = "Potatis";
-let hp = 100;
+const heroName = "Link";
+let hp = 3;
 let isAlive = true;
-const startingInventory = ["Svärd"];
-console.log(`Välkommen ${heroName}! Du börjar med ${hp} HP och ett ${startingInventory}.`);
+const startingInventory = [];
+console.log(`Välkommen hjälten ${heroName}! Du börjar med ${hp} HP.`);
 // -- Steg 2: Inventory --
 // Tomm array som är en string
 const inventory = startingInventory;
-// Du dödade en fiende och hämtar ditt loot
-inventory.push(" Sköld");
-// Du föll för en fälla och tappar det sista i ditt inventory
-// inventory.pop();
-// Du kollar vad du har kvar
-console.log(`Din ryggsäck innehåller: ${inventory}`);
+console.log(`Du går ner i en grotta och stötter på en gammal man, 'Det är farligt att gå ensam, ta det här' `);
+inventory.push("Svärd");
+console.log(`Du fick ett ${inventory}`);
 // -- Steg 3: Äventyret --
-const enemies = ["Slemmpropp", "Varg", "Drake"];
+const enemies = [
+    "Octorok",
+    "Lynel",
+    "Tektite",
+    "Peahat",
+    "Dodongo",
+    "Gohma",
+    "Ganon",
+];
+const enemyDrops = ["Trä sköld", "Boomerang", "Pilbåge", "Bomber"];
+// Enemy skada
+const enemyDmgWeak = 1;
+const enemyDmgStr = 2;
+// Får troligen hårdkoda in värdena på dessa när det sker varje gång, om jag inte listar ut något bättre
+const healingDrops = [
+    "Hjärta",
+    "Hälso dryck",
+    "Hjärta",
+    "Hälso dryck",
+    "Hjärta",
+    "Hälso dryck",
+];
+// Om jag var bättre skulle jag göra så att jag har en variabel som kan återställa hp till max och sen en annan som kan höja max hp
+const hjärta = 1;
 // Försök skriva om den här senare på viset Linus visade
 for (const enemy of enemies) {
-    console.log(`Du möter en: ${enemy}`);
-    if (enemy === "Slemmpropp") {
-        console.log("Du besegrade Slemproppen utan problem!");
-        inventory.push("Guldmynt");
-    }
-    if (enemy === "Varg") {
-        hp -= 20;
-        console.log(`Varjen bet dig! Du har ${hp} HP kvar.`);
-    }
-    if (enemy === "Drake") {
-        if (hp >= 50 && isAlive && inventory.includes("Svärd")) {
-            console.log(`Du besegrade draken! och vann spelet! Du fick en hälsodryck som sätter ditt hp till ${(hp = 100)}`);
+    if (isAlive) {
+        // Hade velat lägga ta bort en: från loggen och typ lägga till det med .map för specifika enemies
+        console.log(`Du möter en: ${enemy}`);
+        if (enemy === "Octorok") {
+            console.log("Du besegrade en Octorok utan problem!");
+            inventory.push(`${enemyDrops[2]}`);
+            inventory.push(`${healingDrops[0]}`);
+            hp += hjärta;
+            console.log(`Du fick en ${enemyDrops[2]} och ett ${healingDrops[0]} som ger dig mer hp. Din hp är nu ${hp}`);
+            inventory.splice(2); // Tar bort hjärtat men är en ganska hårdkodad lösning
         }
-        else {
-            isAlive = false;
-            console.log("Draken sprutade eld. Game Over.");
+        if (enemy === "Lynel") {
+            hp -= enemyDmgWeak;
+            console.log(`Lynel skadade dig! Du har ${hp} HP kvar.`);
+        }
+        if (enemy === "Gohma") {
+            console.log(`Gohma stod inte en chans mot dig`);
+        }
+        if (enemy === "Tektite") {
+            console.log(`Tektite önskar att den hade stött på vad som helst förutom dig.`);
+            inventory.push("Hälso dryck");
+        }
+        if (enemy === "Peahat") {
+            if (inventory.includes("Pilbåge")) {
+                console.log(`Du skjöt ner fienden utan problem!`);
+                inventory.push("Bomber");
+            }
+            else {
+                hp -= enemyDmgStr;
+                console.log(`Du kunde inte skjuta ner den flygande fienden och behövde springa därifrån efter du tagit ${enemyDmgStr} skada, din hp är nu: ${hp}`);
+            }
+        }
+        if (enemy === "Dodongo") {
+            if (inventory.includes("Bomber")) {
+                console.log(`Tur nog hittade du bomber tidigare, du eliminerade Dodongo utan problem med dem`);
+                inventory.splice(3); // Tar bort bomberna
+            }
+            else {
+                hp = 0;
+                isAlive = false;
+                console.log(`Du hade ingen metod för att döda bossen, din hp är ${hp}. Game Over`);
+            }
+        }
+        if (enemy === "Ganon") {
+            if (hp >= 4 ||
+                (inventory.includes("Hälso dryck") && inventory.includes("Svärd"))) {
+                console.log("Du besegrade demonen Ganon! och vann spelet!");
+            }
+            else {
+                isAlive = false;
+                console.log("Du kunde inte ta ner Ganon. Game Over.");
+            }
         }
     }
 }
 // -- Steg 5: Resultat --
 // Om du överlevt ditt äventyr eller inte
-if ((isAlive = true)) {
+if (isAlive === true) {
     console.log(`Grattis ${heroName}, du överlevde äventyret!`);
 }
 else {
-    console.log(`Tyvärr ${heroName}, du dog i skogen...`);
+    console.log(`Tyvärr ${heroName}, du dog...`);
 }
 // Ryggsäckens innehåll i slutet
 console.log(inventory);
-// --- Version 2 av Äventyr --- //
--- src/adventure.ts
+++ src/adventure.ts
@@ -1,67 +1,131 @@
-// --- Version 1 av Äventyr ---
+//*** --- Version 2 av Äventyr --- ***//
+
 // -- Steg 1: Hjälten --
 
-const heroName = "Potatis";
-let hp = 100;
+const heroName = "Link";
+let hp = 3;
 let isAlive = true;
 
-const startingInventory: string[] = ["Svärd"];
+const startingInventory: string[] = [];
 
-console.log(
-  `Välkommen ${heroName}! Du börjar med ${hp} HP och ett ${startingInventory}.`
-);
+console.log(`Välkommen hjälten ${heroName}! Du börjar med ${hp} HP.`);
 
 // -- Steg 2: Inventory --
 // Tomm array som är en string
 const inventory: string[] = startingInventory;
 
-// Du dödade en fiende och hämtar ditt loot
-inventory.push(" Sköld");
-
-// Du föll för en fälla och tappar det sista i ditt inventory
-// inventory.pop();
-
-// Du kollar vad du har kvar
-console.log(`Din ryggsäck innehåller: ${inventory}`);
+console.log(
+  `Du går ner i en grotta och stötter på en gammal man, 'Det är farligt att gå ensam, ta det här' `
+);
+inventory.push("Svärd");
+console.log(`Du fick ett ${inventory}`);
 
 // -- Steg 3: Äventyret --
 
-const enemies: string[] = ["Slemmpropp", "Varg", "Drake"];
+const enemies: string[] = [
+  "Octorok",
+  "Lynel",
+  "Tektite",
+  "Peahat",
+  "Dodongo",
+  "Gohma",
+  "Ganon",
+];
+const enemyDrops: string[] = ["Trä sköld", "Boomerang", "Pilbåge", "Bomber"];
+// Enemy skada
+const enemyDmgWeak = 1;
+const enemyDmgStr = 2;
+// Får troligen hårdkoda in värdena på dessa när det sker varje gång, om jag inte listar ut något bättre
+const healingDrops: string[] = [
+  "Hjärta",
+  "Hälso dryck",
+  "Hjärta",
+  "Hälso dryck",
+  "Hjärta",
+  "Hälso dryck",
+];
+// Om jag var bättre skulle jag göra så att jag har en variabel som kan återställa hp till max och sen en annan som kan höja max hp
+const hjärta = 1;
 // Försök skriva om den här senare på viset Linus visade
 for (const enemy of enemies) {
-  console.log(`Du möter en: ${enemy}`);
+  if (isAlive) {
+    // Hade velat lägga ta bort en: från loggen och typ lägga till det med .map för specifika enemies
+    console.log(`Du möter en: ${enemy}`);
 
-  if (enemy === "Slemmpropp") {
-    console.log("Du besegrade Slemproppen utan problem!");
-    inventory.push("Guldmynt");
-  }
-
-  if (enemy === "Varg") {
-    hp -= 20;
-    console.log(`Varjen bet dig! Du har ${hp} HP kvar.`);
-  }
-
-  if (enemy === "Drake") {
-    if (hp >= 50 && isAlive && inventory.includes("Svärd")) {
+    if (enemy === "Octorok") {
+      console.log("Du besegrade en Octorok utan problem!");
+      inventory.push(`${enemyDrops[2]}`);
+      inventory.push(`${healingDrops[0]}`);
+      hp += hjärta;
       console.log(
-        `Du besegrade draken! och vann spelet! Du fick en hälsodryck som sätter ditt hp till ${(hp = 100)}`
+        `Du fick en ${enemyDrops[2]} och ett ${healingDrops[0]} som ger dig mer hp. Din hp är nu ${hp}`
       );
-    } else {
-      isAlive = false;
-      console.log("Draken sprutade eld. Game Over.");
+      inventory.splice(2); // Tar bort hjärtat men är en ganska hårdkodad lösning
+    }
+
+    if (enemy === "Lynel") {
+      hp -= enemyDmgWeak;
+      console.log(`Lynel skadade dig! Du har ${hp} HP kvar.`);
+    }
+
+    if (enemy === "Gohma") {
+      console.log(`Gohma stod inte en chans mot dig`);
+    }
+
+    if (enemy === "Tektite") {
+      console.log(
+        `Tektite önskar att den hade stött på vad som helst förutom dig.`
+      );
+      inventory.push("Hälso dryck");
+    }
+
+    if (enemy === "Peahat") {
+      if (inventory.includes("Pilbåge")) {
+        console.log(`Du skjöt ner fienden utan problem!`);
+        inventory.push("Bomber");
+      } else {
+        hp -= enemyDmgStr;
+        console.log(
+          `Du kunde inte skjuta ner den flygande fienden och behövde springa därifrån efter du tagit ${enemyDmgStr} skada, din hp är nu: ${hp}`
+        );
+      }
+    }
+
+    if (enemy === "Dodongo") {
+      if (inventory.includes("Bomber")) {
+        console.log(
+          `Tur nog hittade du bomber tidigare, du eliminerade Dodongo utan problem med dem`
+        );
+        inventory.splice(3); // Tar bort bomberna
+      } else {
+        hp = 0;
+        isAlive = false;
+        console.log(
+          `Du hade ingen metod för att döda bossen, din hp är ${hp}. Game Over`
+        );
+      }
+    }
+
+    if (enemy === "Ganon") {
+      if (
+        hp >= 4 ||
+        (inventory.includes("Hälso dryck") && inventory.includes("Svärd"))
+      ) {
+        console.log("Du besegrade demonen Ganon! och vann spelet!");
+      } else {
+        isAlive = false;
+        console.log("Du kunde inte ta ner Ganon. Game Over.");
+      }
     }
   }
 }
 
 // -- Steg 5: Resultat --
 // Om du överlevt ditt äventyr eller inte
-if ((isAlive = true)) {
+if (isAlive === true) {
   console.log(`Grattis ${heroName}, du överlevde äventyret!`);
 } else {
-  console.log(`Tyvärr ${heroName}, du dog i skogen...`);
+  console.log(`Tyvärr ${heroName}, du dog...`);
 }
 // Ryggsäckens innehåll i slutet
 console.log(inventory);
-
-
-// --- Version 2 av Äventyr --- //
